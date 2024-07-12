87aef8f2eb9c013193fc7e561aa936feb12eebb0
Sagar-v4 <sagarvariya4@gmail.com>
2024-07-12 23:28:36 +0530

    feat: add indicator option

--- ts/indicator/list.ts
+++ ts/indicator/list.ts
@@ -3,11 +3,20 @@ import { CHART_INDICATOR_NAME } from "./name";
 export interface ChartIndicator {
   name: CHART_INDICATOR_NAME;
   star: boolean;
+  options: any;
 }
 
 export const INITIAL_CHART_INDICATOR_LIST: ChartIndicator[] = [
   {
     name: CHART_INDICATOR_NAME.MA,
     star: false,
+    options: {
+      type: CHART_INDICATOR_NAME.MA,
+      options: {
+        title: "NEW",
+        interval: 5,
+        format: "close",
+      },
+    },
   },
 ];
